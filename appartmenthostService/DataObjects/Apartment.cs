f5d669cb63e51a463b81e226a9712424efdba67f
deman4ik <deman4ik@gmail.com>
2015-04-05 16:35:22 +0300

    Apartment and Profile models added

---
+++ appartmenthostService/DataObjects/Apartment.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.WindowsAzure.Mobile.Service;
+
+namespace appartmenthostService.DataObjects
+{
+    public class Apartment : EntityData
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string OwnerId { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public decimal PriceTotal { get; set; }
+
+        public string Adress { get; set; }
+
+        public decimal X { get; set; }
+
+        public decimal Y { get; set; }
+
+        public decimal Rating { get; set; }
+
+        public string Сohabitation { get; set; }
+
+        [ForeignKey("OwnerId")]
+        public virtual User Owner { get; set; }
+        public virtual ICollection<Advert> Adverts { get; set; }
+        public virtual ICollection<Picture> Pictures { get; set; } 
+    }
+}
---
+++ appartmenthostService/DataObjects/Profile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.WindowsAzure.Mobile.Service;
+
+
+namespace appartmenthostService.DataObjects
+{
+    public class Profile : EntityData
+    {
+        [Key, ForeignKey("User")]
+        public string UserId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Phone { get; set; }
+        public string ContactEmail { get; set; }
+        public string ContactKind { get; set; }
+        public string Description { get; set; }
+        public string PictureId { get; set; }
+
+        public virtual User User { get; set; }
+        [ForeignKey("PictureId")]
+        public virtual Picture Picture { get; set; }
+    }
+}
