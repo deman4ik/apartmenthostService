aad5356bc687c2240c9e399008456adbce12e04f
deman4ik <deman4ik@gmail.com>
2015-04-06 17:37:02 +0300

    Added Queryable Expand Attribute

--- appartmenthostService/Controllers/AdvertController.cs
+++ appartmenthostService/Controllers/AdvertController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Controllers;
 using System.Web.Http.OData;
+using appartmenthostService.Attributes;
 using Microsoft.WindowsAzure.Mobile.Service;
 using appartmenthostService.DataObjects;
 using appartmenthostService.Models;
@@ -26,6 +27,7 @@ namespace appartmenthostService.Controllers
         }
 
         // GET tables/Advert
+        [QueryableExpand("Apartments")]
         public IQueryable<Advert> GetAllAdverts()
         {
             // Get the logged in user
@@ -36,6 +38,7 @@ namespace appartmenthostService.Controllers
         }
 
         // GET tables/Advert/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        [QueryableExpand("Apartments")]
         public SingleResult<Advert> GetAdvert(string id)
         {
             return Lookup(id);
