20f8b6c562e48defec7a0405b09174a0ee25fa0c
deman4ik <deman4ik@gmail.com>
2015-04-10 10:23:39 +0300

    DTOfix

---
+++ appartmenthostService/DataObjects/ApartmentDTO.cs
@@ -0,0 +1,16 @@
+﻿
+namespace appartmenthostService.DataObjects
+{
+    public class ApartmentDTO
+    {
+        public string Name { get; set; }
+        public string UserId { get; set; }
+        public decimal Price { get; set; }
+        public decimal PriceTotal { get; set; }
+        public string Cohabitation { get; set; }
+        public string Adress { get; set; }
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
+        public decimal? Rating { get; set; } 
+    }
+}
