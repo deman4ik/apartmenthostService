18484b62c218aa1dcb95dd2ea6ba9f9c8139b39a
deman4ik <deman4ik@gmail.com>
2015-04-12 05:23:03 +0300

    Props Fixed

---
+++ appartmenthostService/Models/PropVal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Mobile.Service;
+
+namespace appartmenthostService.Models
+{
+    public class PropVal : EntityData
+    {
+        public string PropId { get; set; }
+        public string TableItemId { get; set; }
+        public string StrValue { get; set; }
+        public decimal? NumValue { get; set; }
+        public DateTime? DateValue { get; set; }
+        public bool BoolValue { get; set; }
+
+        public virtual Prop Prop { get; set; }
+    }
+}
