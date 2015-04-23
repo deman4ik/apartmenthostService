11ec76728f61a9dce9dd74753717bcc290ac6091
deman4ik <deman4ik@gmail.com>
2015-04-23 19:58:09 +0300

    ResponseDTO changed

--- appartmenthostService/DataObjects/ResponseDTO.cs
+++ appartmenthostService/DataObjects/ResponseDTO.cs
@@ -16,29 +16,20 @@ namespace appartmenthostService.DataObjects
     {
         /* Тип ответа
          * Принимает одно из значений класса ResponseTypes */
-        public string Type { get; set; } 
+        public string Code { get; set; } 
 
-        /* Тип объекта вызвашего ошибку */
-        public string Entity { get; set; }
+        /* Список значений */
+        public List<string> Data { get; set; }
 
-        /* Имя поля вызвавшего ошибку */
-        public string Prop { get; set; }
-
-        /* Значение поля вызвавшего ошибку */
-        public string Val { get; set; }
-
-        /* Сообщение
-         * Текст ошибки для пояснений */
-        public string Msg { get; set; }
     }
 
     public static class ResponseTypes
     {
         public const string NULL = "NULL"; // Пустой объект запроса
-        public const string REQUIRED = "REQUIRED"; // Не заполнено обязательно поле, при этом имя поля указывается в Prop, а его текущее значение в Val
-        public const string EXISTS = "EXISTS"; // Объект уже существует, нарушение уникальности по полю в Prop
+        public const string REQUIRED = "REQUIRED"; // Не заполнено обязательно поле
+        public const string EXISTS = "EXISTS"; // Объект уже существует
         public const string UNAUTH = "UNAUTH"; // Пользователь не авторизован
-        public const string NOTFOUND = "NOTFOUND"; // Объект не найден, Тип объекта в Entity, поиск осуществлялся по полю в Prop со значением Val
+        public const string NOTFOUND = "NOTFOUND"; // Объект не найден
         public const string FORBIDDEN = "FORBIDDEN"; // Пользователю запрещено выполнение этого действия
         public const string DEPENDENCY = "DEPENDENCY"; // Объект не может быть изменен/удален т.к. зависит от другого объекта.
     }
