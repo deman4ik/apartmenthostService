1b7a02c55a7a20f6feadd3c0871be6880a1591d1
deman4ik <deman4ik@gmail.com>
2015-10-05 18:10:30 +0300

    Articles without Pictures

--- appartmenthostService/DataObjects/ArticleDTO.cs
+++ appartmenthostService/DataObjects/ArticleDTO.cs
@@ -10,8 +10,6 @@ namespace apartmenthostService.DataObjects
         public string Text { get; set; }
         public string Type { get; set; }
         public string Lang { get; set; }
-        public string PictureId { get; set; }
-        public PictureDTO Picture { get; set; }
         public DateTimeOffset? CreatedAt { get; set; }
         public DateTimeOffset? UpdatedAt { get; set; }
     }
