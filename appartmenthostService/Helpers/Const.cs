84dba2bd524c4a64bfea21fd3dd8b9e81ecede7a
Dmitriy Shatalov <deman4ik@gmail.com>
2015-11-22 19:35:48 +0300

    Changes from master

--- appartmenthostService/Helpers/Const.cs
+++ appartmenthostService/Helpers/Const.cs
@@ -115,7 +115,7 @@ namespace apartmenthostService.Helpers
         public const string SRV_RESERVATION_STATUS_NULL = "SRV_RESERVATION_STATUS_NULL"; //Пустой объект запроса
         public const string SRV_RESERVATION_NOTFOUND = "SRV_RESERVATION_NOTFOUND"; // Объект не найден
         public const string SRV_RESERVATION_EXISTS = "SRV_RESERVATION_EXISTS"; // Объект уже существует
-
+        public const string SRV_RESERVATION_SELF = "SRV_RESERVATION_SELF"; // Нельзя бронировать свое же жилье
         public const string SRV_RESERVATION_WRONG_DATE = "SRV_RESERVATION_WRONG_DATE";
         // Дата С должна быть меньше Даты ПО
 
