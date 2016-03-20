a763f5bcf4bbe8c0ac3f4ceac7f16e0b4f7c3e0f
Dmitriy Shatalov <deman4ik@gmail.com>
2016-03-20 22:38:49 +0300

    Pagination

---
+++ appartmenthostService/DataObjects/CardResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace apartmenthostService.DataObjects
+{
+    public class CardResponseDTO
+    {
+        public int Count { get; set; }
+        public List<CardDTO> Cards { get; set; } 
+    }
+}
